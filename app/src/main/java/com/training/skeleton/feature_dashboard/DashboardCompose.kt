1d2a3afeadfc691f6eef035cad0fd4f2889f8b99
Ravindra Shekhawat <ravindrashekhawat20@gmail.com>
2023-04-24 23:28:58 +0530

    Added support for animelist handling from viewmodel

--- app/src/main/java/com/training/skeleton/feature_dashboard/DashboardCompose.kt
+++ app/src/main/java/com/training/skeleton/feature_dashboard/DashboardCompose.kt
@@ -82,7 +82,8 @@ fun ShowAnimeList(animeList:List<Anime>){
                 )
                 Text(
                     text = animeList[index].title.text,
-                    textAlign = TextAlign.Center
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.size(12.dp)
                 )
             }
         }
@@ -102,96 +103,5 @@ fun ShowAnimeList(){
     ShowAnimeList(animeList = animeList )
 }
 
-var animeList= listOf<Anime>(
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
-    Anime(
-        "Nakajima Atsushi was kicked out of his orphanage",
-        listOf(
-            "Mystery",
-            "Seinen",
-            "Supernatural"
-        ),
-        12345,
-        "Apr 7, 2016, 01:05 (JST)", "Bones",
-        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
-    ),
 
-
-    )
 
--- app/src/main/java/com/training/skeleton/feature_dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/training/skeleton/feature_dashboard/DashboardViewModel.kt
@@ -1,6 +1,111 @@
 package com.training.skeleton.feature_dashboard
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.training.skeleton.feature_dashboard.data.Anime
+import com.training.skeleton.feature_dashboard.data.Title
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
+data class AnimeUIState(
+    var animeList:List<Anime> = mutableListOf()
+)
 class DashboardViewModel:ViewModel() {
+    private val _animeUIState= MutableStateFlow(AnimeUIState())
+    val animeUIState:StateFlow<AnimeUIState> = _animeUIState.asStateFlow()
+
 }
+
+var animeList= listOf<Anime>(
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+    Anime(
+        "Nakajima Atsushi was kicked out of his orphanage",
+        listOf(
+            "Mystery",
+            "Seinen",
+            "Supernatural"
+        ),
+        12345,
+        "Apr 7, 2016, 01:05 (JST)", "Bones",
+        Title("https://cdn.myanimelist.net/images/anime/1077/124345.jpg", "Bungou Stray Dogs")
+    ),
+
+
+    )
