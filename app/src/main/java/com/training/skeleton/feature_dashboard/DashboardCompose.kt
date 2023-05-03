dfc6f43961cc99d0f9b6dc9ee4fd2238c535a513
Ravindra Shekhawat <ravindrashekhawat20@gmail.com>
2023-05-03 23:16:09 +0530

    Added support for ansync images and get it from api

--- app/src/main/java/com/training/skeleton/feature_dashboard/DashboardCompose.kt
+++ app/src/main/java/com/training/skeleton/feature_dashboard/DashboardCompose.kt
@@ -1,28 +1,33 @@
 package com.training.skeleton.feature_dashboard
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Shapes
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.training.skeleton.MainActivityViewModel
-import com.training.skeleton.feature_dashboard.data.Anime
 import com.training.skeleton.feature_dashboard.data.DasboardViewModelFactory
-import com.training.skeleton.feature_dashboard.data.Product
 import com.training.skeleton.feature_dashboard.data.ProductDetail
 import com.training.skeleton.navigation.Screen
 import com.training.skeleton.repository.ProductRepository
@@ -62,21 +67,13 @@ fun DashboardMainContent(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally){
 
-
-        Button(onClick = { navigateToProfile()}) {
-            Text(text = "Go to profile")
-        }
-
-        Button(onClick = { navigateToSettings()}) {
-            Text(text = "Go to settings")
-        }
-        ShowAnimeList(productList)
+        ShowProductList(productList)
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ShowAnimeList(productList:List<ProductDetail>){
+fun ShowProductList(productList:List<ProductDetail>){
 
     LazyVerticalGrid(cells = GridCells.Fixed(2)) {
         items(productList.size) { index ->
@@ -89,11 +86,17 @@ fun ShowAnimeList(productList:List<ProductDetail>){
                         .data(productList[index].thumbnail)
                         .build(),
                     contentDescription = null,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier.size(128.dp)
+                    .padding(16.dp)
+                    .clip(CircleShape)                       // clip to the circle shape
+                    .border(1.dp, Color.Black, CircleShape)
                 )
                 Text(
                     text = productList[index].title,
-                    textAlign = TextAlign.Center
+                    textAlign = TextAlign.Center,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold
                 )
             }
         }
@@ -109,8 +112,8 @@ fun DashboardPreview(){
 
 @Composable
 @Preview
-fun ShowAnimeList(){
-    ShowAnimeList( )
+fun ShowProductList(){
+    ShowProductList( )
 }
 
 
